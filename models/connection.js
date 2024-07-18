b8c5e5bad5e4874b8e130e81ea0aa70c541b91bb
nive <nivi2384@gmail.com>
2024-07-18 19:58:58 -0400

    connection and gitignore file modified

--- models/connection.js
+++ models/connection.js
@@ -1,15 +1,16 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-});
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    }
+  );
 
 module.exports = sequelize;
