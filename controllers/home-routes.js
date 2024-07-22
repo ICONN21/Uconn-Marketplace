d1350aa2012e98cb8e7ebfb0ed3338da217ad8a6
Ian Connor <Ian.connor0921@gmail.com>
2024-07-22 15:17:43 -0400

    re-worked some files to follow MVC complience better. Also fixed er ror with server started up regarding MIME on the home page. Still recieving some erros in the console on the post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,10 +10,10 @@ router.get('/', async (req, res) => {
     // Debugging: Log itemData to see what it contains
     console.log('itemData:', itemData);
 
-    if (!itemData || itemData.length === 0) {
-      // If itemData is null, undefined, or empty, send an error response
-      return res.status(404).json({ message: 'No items found' });
-    }
+    // if (!itemData || itemData.length === 0) {
+    //   // If itemData is null, undefined, or empty, send an error response
+    //   return res.status(404).json({ message: 'No items found' });
+    // }
 
     const items = itemData.map((item) => item.get({ plain: true }));
     res.render('home', { items });
